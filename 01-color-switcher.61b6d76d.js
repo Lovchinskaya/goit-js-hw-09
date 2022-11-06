bf44538a6becc1188772304f3e8f7d5605ac4353
Lovchinskaya <33342414+Lovchinskaya@users.noreply.github.com>
2022-11-06 15:29:42 +0000

    Deploying to gh-pages from @ Lovchinskaya/goit-js-hw-09@7c1fddc26457ab5665d47801755e45a1e52bc9ab 🚀

---
+++ 01-color-switcher.61b6d76d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){console.log("it works")}))}();
+//# sourceMappingURL=01-color-switcher.61b6d76d.js.map
---
+++ 01-color-switcher.6eb38ce8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){console.log("it works")}));
+//# sourceMappingURL=01-color-switcher.6eb38ce8.js.map
