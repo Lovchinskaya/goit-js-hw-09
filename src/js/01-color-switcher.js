3f5a450684ef17fb8ad7c3b4c1fa9252ed5a234b
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-11-06 18:08:18 +0200

    HW9 1task ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,27 @@
 
 const start = document.querySelector('[data-start]');
 const stop = document.querySelector('[data-stop]');
+let intervalID;
+start.addEventListener('click', changeBGC);
+stop.addEventListener('click', deleteInterval);
 
-start.addEventListener('click', startRandonColor);
+function getRandomHexColor() {
+   let rgb = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${rgb}`;
+}
 
-function startRandonColor() {
-    console.log('it works')
+function changeBGC() {
+    start.disabled = true;
+    stop.disabled = false;
+    intervalID = setInterval(() => {
+        console.log(getRandomHexColor());
+        document.body.style.backgroundColor = `${getRandomHexColor()}`;
+
+    }, 1000); 
+}
+
+function deleteInterval() {
+    clearInterval(intervalID);
+    start.disabled = false;
+    stop.disabled = true;
 }
