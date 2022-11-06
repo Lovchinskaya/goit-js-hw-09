7c1fddc26457ab5665d47801755e45a1e52bc9ab
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-11-06 17:29:05 +0200

    HW 9 1 commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
 
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+
+start.addEventListener('click', startRandonColor);
+
+function startRandonColor() {
+    console.log('it works')
+}
