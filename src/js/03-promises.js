47412a34990e1471d4ef41fa4f3d1b8d9a076522
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-11-07 14:12:56 +0200

    HW9 3task in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,57 @@
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector(['delay']);
+const step = document.querySelector(['step']);
+const amount = document.querySelector(['amount']);
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', createPromise);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    //Notiflix.Notify.success('Success');
   } else {
     // Reject
+    Notiflix.Notify.failure('Failure');
   }
 }
+createPromise(position, delay)
+ .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+//}
+
+//function createPromise(position, delay) {
+ // const shouldResolve = Math.random() > 0.3;
+ // for (let i = 0; i > amount.value; i = +1) {
+ //   setTimeout(() => {
+ //     setInterval(() => {
+ //       const result = () => {
+          if (shouldResolve) {
+            // Fulfill
+            Notiflix.Notify.success('Success');
+          } else {
+            // Reject
+            Notiflix.Notify.failure('Failure');
+          }
+ //       }
+ //     }, step)
+ //   }, delay);
+ //   console.log(result);
+ // }
+ // return result;
+//}
+//}
+
+
+//1. delay = timeout; step = interval; amount = i
+//2. circle
+//3. if / else  success/faliure
