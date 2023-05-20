2a5df999530f63d38126b9129a2cc86d717578d0
Lovchinskaya <33342414+Lovchinskaya@users.noreply.github.com>
2023-05-20 17:51:37 +0300

    HW JS 9
    First Commit 20.05

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -37,42 +37,4 @@ refs.addEventListener("submit", (e)=> {
 });
 
  
- // if (shouldResolve) {
-   // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    // Fulfill
- //   Notiflix.Notify.success('Success');
-//  } else {
- //   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    // Reject
-//    Notiflix.Notify.failure('Failure');
-// }
-
-
-  
-
-//function createPromise(position, delay) {
- // const shouldResolve = Math.random() > 0.3;
- // for (let i = 0; i > amount.value; i = +1) {
- //   setTimeout(() => {
- //     setInterval(() => {
- //       const result = () => {
-         // if (shouldResolve) {
-         // Fulfill
-          //  Notiflix.Notify.success('Success');
-          //} else {
-            // Reject
-          //  Notiflix.Notify.failure('Failure');
-         // }
- //       }
- //     }, step)
- //   }, delay);
- //   console.log(result);
- // }
- // return result;
-//}
-//}
-
-
-//1. delay = timeout; step = interval; amount = i
-//2. circle
-//3. if / else  success/faliure
+ 
